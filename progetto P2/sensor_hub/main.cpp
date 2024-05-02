bcaf15af3afc97e1bfa2444432546ccb1421075c
albertocnve1 <158286084+albertocnve1@users.noreply.github.com>
2024-05-03 00:22:14 +0200

    Add files via upload

---
+++ progetto P2/sensor_hub/main.cpp
@@ -0,0 +1,23 @@
+#include "mainwindow.h"
+
+#include "sensor.h"
+#include "temperature_sensor.h"
+#include "dust_sensor.h"
+#include "humidity_sensor.h"
+#include "load_sensors.h"
+#include "mainwindow.h"
+
+#include <QApplication>
+#include <QVBoxLayout>
+#include <QListWidget>
+#include <QLabel>
+
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+    MainWindow window;
+    window.show();
+
+    return app.exec();
+}
